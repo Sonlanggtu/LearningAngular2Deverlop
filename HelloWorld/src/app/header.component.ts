e64eb3e6026853d2c9474d2ba1ff503aac16a05e
DESKTOP-VO37R9T\Sonlanggtu <Sonlanggtu@gmail.com>
2019-02-03 01:30:44 +0700

    this is reponsitory lession12

--- HelloWorld/src/app/header.component.ts
+++ HelloWorld/src/app/header.component.ts
@@ -4,31 +4,28 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'App-Header',
     template : `
-    <p *ngIf = 'TestIf'>Đây là thẻ test ngIf</p>
-
-    <div [ngSwitch] = "TestCars">
-            <b>Category Cars</b>
-    <p *ngSwitchCase = "'S500'">Mecedes S500</p>
-    <p *ngSwitchCase = "'Camry'">Camry 2.0E</p>
-    <p *ngSwitchCase = "'E250'">Mecedes E250</p>
-    <p *ngSwitchCase = "'C200'">Mecedes C200</p>
-    <p *ngSwitchCase = "'HondaRecord'">Honda Record</p>
-    <div>
-    
-    For Directives
-    <ul>
-        <li *ngFor = "let Flower of StringFlower">{{Flower}} </li>
-    </ul>
+    <p [ngClass] = "{FontRed:TestFontRed,BackgroundBlue:TestbackgroundBlue }"> This is Tag P </p>
+    <p [ngStyle] = "{'font-weight':800}">Ap dung ngStyle</p>
     `,
-    styles : [``]
+    styles : [`
+        .FontRed{
+            color : red;
+        }
+        .BackgroundBlue{
+            background-color:blue;
+            width:200px;
+            height: 100px;
+        }
+
+    `]
     // templateUrl: './header.component.html',
     // styleUrls: ['./header.component.css'],
 })
 
 export class AppHeader{
     ClassEPU = "D11QTANM";
-    public TestCars = "S500";
-    public TestIf = false;
-
-    public StringFlower : string[] = ['Lan','Cuc','Hue','DongTien','HuongDuong'];
+    TestFontRed = false;
+    TestbackgroundBlue = true;
+    TestNGStyle = true;
+   
 }
