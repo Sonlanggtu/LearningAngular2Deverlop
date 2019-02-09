71c7438465eeae8572fe7bd7aaa3ab1d4242058e
DESKTOP-VO37R9T\Sonlanggtu <Sonlanggtu@gmail.com>
2019-02-09 14:06:13 +0700

    lession 17 validate forms part 2

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -3,13 +3,20 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'SonLanggtu';
-    public Form_InfoEnpolyee(value : any){
-      console.log(value);
-    }
-
-
+  public Form_InfoEnpolyee(value: any) {
+    console.log(value);
   }
+  public city = [
+    {ID:1, Name:'Hà Nội'},
+    {ID:2, Name:'TP. HCM'},
+    {ID:3, Name:'Hải Phòng'},
+    {ID:4, Name:'Đà Nẵng'},
+    {ID:5, Name:'Cần Thơ'},
+    {ID:6, Name: 'Hưng Yên'},
+    {ID:7, Name:'Hải Dương'}
+]
+}
